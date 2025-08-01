f34b5796df0078ba895afbd4ebedac89c286b02b
Alan Hsieh <twcch1218@gmail.com>
2025-08-01 21:45:20 +0800

    更新 day 2 的程式碼

--- day02_linear_regression.py
+++ day02_linear_regression.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
