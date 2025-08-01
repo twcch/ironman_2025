87fa0771e249a863abcda8a07f9a70798ebbcab5
Alan Hsieh <twcch1218@gmail.com>
2025-08-01 23:11:20 +0800

    更新 day 3 的程式碼

--- day03_polynomial_regeression.py
+++ day03_polynomial_regeression.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
